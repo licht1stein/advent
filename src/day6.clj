aa80630ff93193845d4c644cf50ab55754b00721
MB <mb@blaster.ai>
2022-12-11 00:34:20 +0100

    kw instead of multiarity

--- src/day6.clj
+++ src/day6.clj
@@ -3,12 +3,10 @@
             [clojure.java.io :as io]))
 
 (defn solve
-  ([s]
-   (solve s 4))
-  ([s n]
-   (loop [i 0]
-     (if (= n (->> s (take i) (take-last n) set count))
-       i (recur (inc i))))))
+  [s & {:keys [n] :or {n 4}}]
+  (loop [i 0]
+    (if (= n (->> s (take i) (take-last n) set count))
+      i (recur (inc i)))))
 
 
 (def data (slurp (io/resource "day6.txt")))
